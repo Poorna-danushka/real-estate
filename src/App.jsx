3daf7e040213550d65a2308b64712e62e4c0b25d
Poorna-danushka <poornadanushka2@gmail.com>
2025-12-06 11:18:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+     <h1 className="text-4xl font-bold text-red-500">
+      Tailwind is working!
+    </h1>
+  )
+}
+
+export default App
